4377fc842693aebc6cf14065f0f4f4d971e0627d
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-24 20:02:25 -0400

    made more changes

---
+++ client/src/components/LandingGrid/LandingGrid.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+export default function FixedContainer() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Grid />
+      </Container>
+    </React.Fragment>
+  );
+}
