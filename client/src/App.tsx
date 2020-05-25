8dc5370960528757b48d439e141f5d1b017f23de
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-25 14:15:52 -0400

    landing grid with cards works?

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <SearchAppBar />
-      <Jumbotron />
       <LandingGrid />
     </div>
   );
--- client/src/components/LandingGrid/LandingGrid.tsx
+++ client/src/components/LandingGrid/LandingGrid.tsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import Grid, { GridSpacing } from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }),
+);
+
 
 export default function LandingGrid() {
+
+  const [spacing, setSpacing] = React.useState<GridSpacing>(2);
+  const classes = useStyles();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSpacing(Number((event.target as HTMLInputElement).value) as GridSpacing);
+  };
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fixed>
-        <Grid />
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item md={2}>
+            <Grid container justify="center" spacing={spacing}>
+              {[0, 1, 2].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
     </React.Fragment>
   );
