2f06c1f18d252cb0ae31ed744fcd2f6a9c4c188a
jenniferkirwin <lamontagneartist@gmail.com>
2020-05-27 19:13:00 -0400

    Updated home page to fix start error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
+// Dependencies
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 import express from 'express';
 const PORT = process.env.PORT || 3500;
 
 const app = express();
 app.use(express.json());
 
+// Getting sequelize for database
+// const db = require("./models");
+
+// Routes
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+// Connects to database then starts the server
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}`);
+//   });
+// });
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
