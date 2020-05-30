0549a8cff6d76201501a8131aab77c0f0bfec807
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-30 10:08:47 -0400

    Made an upload assignment page

---
+++ client/src/pages/Main/UploadAssign.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TeacherPage from './TeacherAssignmentPage/LandingGrid/index';
+import TeacherAppBar from './TeacherAssignmentPage/NavBar/index';
+
+const UploadAssign = () => {
+    return (
+      <div>
+          <TeacherAppBar />
+          <TeacherPage />
+      </div>  
+    );
+}
+
+export default UploadAssign;
