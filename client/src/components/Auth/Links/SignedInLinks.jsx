0336c49b32a297663e87ab7236989737956b4a15
OliviaL18 <oliviatloos@gmail.com>
2020-06-01 23:01:26 -0400

    log out functionality

--- client/src/components/Auth/Links/SignedInLinks.jsx
+++ client/src/components/Auth/Links/SignedInLinks.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import app from "./../../../config/fbConfig.js";
+
 
 const SignedInLinks = () => {
   return (
     <div>
       <ul className="right">
-      <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/createaccount'>Create Account</NavLink></li>
-        <li><NavLink to='/'>Log Out</NavLink></li>
+        <button className="btn waves-effect waves-light btn-small lighten-1 z-depth-0" onClick={() => app.auth().signOut()}>Sign out</button>
       </ul>
     </div>
   )
