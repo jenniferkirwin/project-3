5e7cbca3ed59c070a3f3bcb7b68a874592a5f486
jenniferkirwin <lamontagneartist@gmail.com>
2020-06-07 18:57:32 -0400

    Creating teacher routes

--- client/functions/controllers/teacher-controller.js
+++ client/functions/controllers/teacher-controller.js
@@ -5,74 +5,15 @@
 const db = require('../models');
 const hr = '-------------------------------------------------'
 
-// Functions
-// ---------------------------------------------------------------------------
-// ---------------------------------------------------------------------------
-let returnedSchoolId;
-let returnedUserId;
-
-const foundSchool = (school) => {
-  return db.School
-  .findOne({
-    where: {
-      schoolName: school,
-    },
-    attributes: ['schoolId'],
-    raw: true
-  })
-  .then((foundSchoolObj) => {
-    returnedSchoolId = foundSchoolObj.schoolId;
-    return foundSchoolObj.schoolId;
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-}
-
-const foundUser = (user) => {
-  return db.User
-  .findOne({
-    where: {
-      email: user,
-    },
-    attributes: ['userId'],
-    raw: true
-  })
-  .then((foundUserObj) => {
-    returnedUserId = foundUserObj.userId;
-    return foundUserObj.userId;
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-}
-
-const varReset = () => {
-  returnedSchoolId = '';
-  returnedUserId = '';
-}
-
-
 // Routes
 // ---------------------------------------------------------------------------
 // ---------------------------------------------------------------------------
 
 module.exports = {
-  findCourses: (req, res) => {
-    db.Course
-    .findAll({
-      where: {
-        UserUserId: req.params.id
-      }
-    })           
-    .then((foundCourses) => {
-      res.status(200).json(foundCourses);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.sendStatus(500);
-    })
-  },
+
+  // Create Courses Functions
+  // -------------------------------------------------------------------------
+
   createCourse: (req, res) => {
     db.Course
     .create({
@@ -87,13 +28,82 @@ module.exports = {
       console.error(error);
       res.sendStatus(500);
     })
+  },
+
+  findCourses: (req, res) => {
+    db.Course
+    .findAll({
+      where: {
+        UserUserId: req.params.teacherId
+      }
+    })           
+    .then((foundCourses) => {
+      res.status(200).json(foundCourses);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    })
+  },
+
+  // Enroll and Find Student Functions
+  // -------------------------------------------------------------------------
+
+  enrollStudent: (req, res) => {
+    db.Enrollment
+    .create({
+      CourseCourseId: req.body.courseId,
+      UserUserId: req.body.userId        
+    })
+    .then((newEnrollment) => {
+      res.status(200).json(newEnrollment);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    })
+  },
+
+  findStudents: (req, res) => {
+    db.Course
+    .findAll({
+      where: {
+        UserUserId: req.params.teacherId
+      },
+      include: [{
+        model: db.Enrollment,
+        include: [{
+          model: db.User,
+        }]
+      }]
+    })           
+    .then((foundStudents) => {
+      res.status(200).json(foundStudents);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    })
+  },
+
+  // Enroll and Find Student Functions
+  // NOT DONE YET... STILL WORKING ON
+  // -------------------------------------------------------------------------
+
+  createAssignment: (req, res) => {
+    db.Assignment
+    .create({
+      assignmentText: req.body.assignmentText,
+      dueDate: req.body.dueDate,
+      CourseCourseId: req.body.courseId        
+    })
+    .then((newCourse) => {
+      res.status(200).json(newCourse);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    })
   }
-  // updateClass: (req, res) => {
-  //   db.Class
-  //   .create({
-  //     courseName: req.body.courseName,
-  //     SchoolSchoolId: req.body.lastName,
-  //     UserUserId: req.body.email        
-  //   }) 
-  // }
+
 };
--- client/functions/routes/teacher-routes.js
+++ client/functions/routes/teacher-routes.js
@@ -10,12 +10,44 @@ const userController = require('../controllers/teacher-controller');
 // ---------------------------------------------------------------------------
 // ---------------------------------------------------------------------------
 
+// Create Courses
 router
   .route('/courses')
   .post(userController.createCourse)
 
+// Courses by TeacherID
 router
-  .route('/courses/:id')
+  .route('/courses/:teacherId')
   .get(userController.findCourses)
 
+// Create Student Enrollment
+router
+  .route('/enrollments/')
+  .post(userController.enrollStudent)
+
+// Find Students in Given Teacher's Classes
+router
+  .route('/enrollments/:teacherId')
+  .get(userController.findStudents)
+
+router
+  .route('/assignments/')
+  .post(userController.createAssignment)
+  // .put(userController.updateAssignment)
+  // .get(userController.findAssignments)
+
+// router
+//   .route('/assignments/:studentId')
+//   .get(userController.findStudentAssignments)
+
+// // NEED TO THINK ABOUT THIS ROUTE
+// router
+//   .route('/assignments/submitted/:studentId')
+//   .put(userController.updateStudentAssignment)
+
+
 module.exports = router;
+
+// find students
+// find assignments
+// create assignments
