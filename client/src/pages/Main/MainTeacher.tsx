b8ef0491d4439be5ce8941e1f7bb772e968cea01
OliviaL18 <oliviatloos@gmail.com>
2020-06-10 20:02:31 -0400

    removed extra nav bars (overlap w. main nav bar)

--- client/src/pages/Main/MainTeacher.tsx
+++ client/src/pages/Main/MainTeacher.tsx
@@ -1,40 +1,13 @@
 import React , { useContext } from 'react';
 import clsx from 'clsx';
-import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import HomeIcon from '@material-ui/icons/Home';
-import GradeIcon from '@material-ui/icons/Grade';
-import ClassIcon from '@material-ui/icons/Class';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import ClassHoursCard from './TeacherAssignmentPage/Card/ClassHours';
 import GradesCard from './TeacherAssignmentPage/Card/GradesCard';
 import TeacherAssingmentCard from './TeacherAssignmentPage/Card/index';
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import SignedInLinks from './../../components/Auth/Links/SignedInLinks.jsx';
-import SignedOutLinks from './../../components/Auth/Links/SignedOutLinks.jsx';
-import { AuthContext } from './../../components/Auth/auth.js';
-import { NavLink } from 'react-router-dom';
-
-
-const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -57,14 +30,6 @@ const useStyles = makeStyles((theme) => ({
             duration: theme.transitions.duration.leavingScreen,
         }),
     },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
     menuButton: {
         marginRight: 36,
     },
@@ -73,26 +38,6 @@ const useStyles = makeStyles((theme) => ({
     },
     title: {
         flexGrow: 1,
-    },
-    drawerPaper: {
-        position: 'relative',
-        whiteSpace: 'nowrap',
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerPaperClose: {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9),
-        },
     },
     appBarSpacer: theme.mixins.toolbar,
     content: {
@@ -118,105 +63,13 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const menuItems = [
-    {
-        listIcon: <HomeIcon />,
-        listText: "Teacher Home",
-        listPath: "/teacher-home",
-      },
-      {
-        listIcon: <AssignmentIcon />,
-        listText: "Teacher Assignments",
-        listPath: "/teacher-upload-assignment",
-      },
-      {
-        listIcon: <GradeIcon />,
-        listText: "Teacher Grade View",
-        listPath: "/teacher-grades",
-      },
-      {
-        listIcon: <AccessTimeIcon />,
-        listText: "Teacher Class Hours",
-        listPath: "/teacher-class-hours",
-      }
-]
-
-let greetingString;
-let userEmail = "";
-
-
 export default function Dashboard() {
     const classes = useStyles();
-
-    const { currentUser } = useContext(AuthContext);
-    //Set the greeting string
-    if (currentUser) {
-      userEmail = currentUser.email;
-      greetingString = `Welcome, ${userEmail}`;
-    } else {
-      greetingString = `Welcome to Skooled! Please Sign In or Create an Account`; 
-    }
-
-    const [open, setOpen] = React.useState(true);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
     return (
         <div className={classes.root}>
             <CssBaseline />
-            <AppBar
-            position="fixed"
-            className={clsx(classes.appBar, {
-              [classes.appBarShift]: open,
-            })}
-          >
-            <Toolbar>
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={handleDrawerOpen}
-                    className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                >
-                <MenuIcon />
-            </IconButton>
-              <NavLink activeStyle={{color: 'white', textDecoration: 'none'}} to='/'>
-                <Typography variant="h6" noWrap>
-                  { greetingString }
-                </Typography>
-              </NavLink>
-              <SignedInLinks />
-            </Toolbar> 
-          </AppBar>
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-                }}
-                open={open}
-            >
-                <div className={classes.toolbarIcon}>
-                    <IconButton onClick={handleDrawerClose}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>
-                    {menuItems.map((lsItem, key) => (
-                        <ListItem button key={key} component={Link} to={lsItem.listPath}>
-                            <ListItemIcon className={classes.listItem}>
-                                {lsItem.listIcon}
-                            </ListItemIcon>
-                            <ListItemText className={classes.listItem} primary={lsItem.listText} />
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
             <main className={classes.content}>
                 <div className={classes.appBarSpacer} />
                 <Container maxWidth="lg" className={classes.container}>
