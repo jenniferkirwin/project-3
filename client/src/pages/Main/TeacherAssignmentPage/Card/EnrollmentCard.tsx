e86649a8b014d6e74a2efcf5dec7cf28198947d9
Nathan Sanford <nwsanfor24@gmail.com>
2020-06-13 11:29:02 -0400

    delted some files and made a new enrollment field/card

---
+++ client/src/pages/Main/TeacherAssignmentPage/Card/EnrollmentCard.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 300,
+        minHeight: 200,
+        marginBottom: '16px',
+        marginTop: '10px',
+        marginRight: '20px',
+        marginLeft: '20px',
+    },
+    media: {
+        height: 140,
+    },
+    title: {
+        backgroundColor: "darkblue",
+        fontsize: 14,
+        color: 'white',
+        padding: theme.spacing(2),
+        borderTop: '2px solid darkblue',
+    },
+    size: {
+        width: "100%",
+        textAlign: "center",
+    },
+    pos: {
+        marginBottom: 12,
+    },
+}));
+
+export default function EnrollmentCard() {
+
+    const classes = useStyles();
+
+    return(
+        <div>
+
+        </div>
+    )
+
+}
+
+// {submitted.map((submit) => (
+//     <Card className={classes.root}>
+//         <CardContent>
+//             <div key={submit.id}>
+//                 <Typography className={classes.title} gutterBottom variant="h5" component="h2">
+//                     {submit.submittedId}
+//                 </Typography>
+//                 <Typography variant="body2" color="textSecondary" component="p">
+//                     {submit.submittedContent}
+//                 </Typography>
+//                 <h3>{submit.grade}</h3>
+//             </div>
+//         </CardContent>
+//         <CardActions>
+//             <Button size="small">Grades</Button>
+//         </CardActions>
+//     </Card>
+// ))}
--- client/src/pages/Main/TeacherAssignmentPage/LandingGrid/EnrollmentGrid.tsx
+++ client/src/pages/Main/TeacherAssignmentPage/LandingGrid/EnrollmentGrid.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import clsx from 'clsx';
 import {
     makeStyles,
     createStyles,
     Theme
 } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import GradesCard from '../Card/GradesCard';
 import Container from '@material-ui/core/Container';
-
-
+import EnrollmentCard from '../Card/EnrollmentCard';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -29,45 +26,34 @@ const useStyles = makeStyles((theme: Theme) =>
             paddingTop: theme.spacing(4),
             paddingBottom: theme.spacing(4),
         },
-        paper: {
-            padding: theme.spacing(2),
-            display: 'flex',
-            overflow: 'auto',
-            flexDirection: 'column',
-        },
         fixedHeight: {
             height: 240,
         },
     }),
 );
 
-export default function GradesGrid() {
+export default function EnrollmentGrid() {
 
     const classes = useStyles();
 
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-
     return (
         <div>
-            
-                <main className={classes.content}>
+            <main className={classes.content}>
                 
-                    <div className={classes.appBarSpacer} />
-                    <Container maxWidth="lg" className={classes.container}>
-                        <Grid container spacing={3}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    <Grid container spacing={3}>
 
-                            <Grid item xs={12} md={8} lg={9}>
+                        <Grid item xs={12} md={8} lg={9}>
 
-                                <GradesCard />
-
-                            </Grid>
+                            <EnrollmentCard />
 
                         </Grid>
-                    </Container>
-                </main>
-            
 
-
+                    </Grid>
+                </Container>
+            </main>
         </div>
-    );
+    )
+
 }
